953e76066599879f12e6b67e943ae5956ccfae7a
Stoica Denisa <teodora202@yahoo.com>
2022-11-30 23:17:53 +0200

    lab8

--- Stoica_Denisa_Lab7/App.xaml.cs
+++ Stoica_Denisa_Lab7/App.xaml.cs
@@ -1,8 +1,24 @@
 ﻿namespace Stoica_Denisa_Lab7;
-
+using System;
+using Stoica_Denisa_Lab7.Data;
+using System.IO;
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+                ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
 	{
 		InitializeComponent();
 
